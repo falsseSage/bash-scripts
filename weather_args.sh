f96c8cbd310b1f0f02684174c763ef6ba25b9608
True Sarmiento <true.sarmiento@gmail.com>
2023-02-11 12:52:10 -0800

    init commit
    basic cal and weather CLI app
    uses args to customize location, format, and refresh time

---
+++ weather_args.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# takes args city, wttr format type, and refresh interval
+
+city=$1
+format=$2
+refresh=$3
+
+echo -e "Welcome back, $USER!"
+
+# sets date to m/d/Y format
+date=$(date +'%D')
+cal=$(cal)
+
+echo -e "Today is $date"
+echo -e "$cal"
+
+# keep while loop running for weather update
+while true
+do
+  weather=$(curl -s http://wttr.in/$1?format=$2)
+  echo "$weather"
+  sleep $3
+done
